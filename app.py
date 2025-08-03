b5f72acfe7e87b24b1c9be7c14ace749dd6b2ba3
Ameny <amenybenamor422@gmail.com>
2025-08-03 01:31:28 +0100

    mise a jours

--- app.py
+++ app.py
@@ -3,35 +3,42 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
 import numpy as np
 import os
-
+import json
 app = Flask(__name__)
 
 MODEL_PATH = "model/best_model.h5"
 model = tf.keras.models.load_model(MODEL_PATH)
 
-class_names = [
-    "Colorectal cancer", 
-    "Ulcer", 
-    "Esophagitis", 
-    "Gastric polyps", 
-    "Mucosal inflammation",
-    "Normal mucosa", 
-    "Normal stomach", 
-    "Pylorus"
-]
+
+
+with open("class_indices.json", "r") as f:
+    class_indices = json.load(f)
+
+# Inverser le mapping
+idx_to_class = {v: k for k, v in class_indices.items()}
 
 def predict_image(image_path):
     img = load_img(image_path, target_size=(224, 224))
     img_array = img_to_array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
     prediction = model.predict(img_array)[0]
-    predicted_index = np.argmax(prediction)
-    predicted_label = class_names[predicted_index]
+    predicted_index = int(np.argmax(prediction))
+    predicted_label = idx_to_class[predicted_index]
 
-    if predicted_label in ["Colorectal cancer", "Ulcer", "Esophagitis", "Gastric polyps", "Mucosal inflammation"]:
+    # Liste des classes considérées comme maladies
+    maladies = [
+        "Colorectal cancer",
+        "Esophagitis",
+        "Gastric polyps",
+        "Mucosal inflammation large bowel",
+        "Ulcer"
+    ]
+
+    if predicted_label in maladies:
         return f"malade : {predicted_label}"
     else:
         return "sain"
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
